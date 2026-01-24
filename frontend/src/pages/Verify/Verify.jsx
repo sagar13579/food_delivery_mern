5c5ad4c16ce3978ce28ec70fdc653a16a8e36dfc
Sagar Ghosh <dinkoghosh@gmail.com>
2026-01-25 00:06:10 +0530

    used axios instead of axios instance

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,8 +1,8 @@
-import axios from 'axios';
 import { useContext, useEffect } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { StoreContext } from '../../Context/StoreContext';
 import './Verify.css'
+import axiosInstance from '../../axiosInstance';
 
 const Verify = () => {
   const { url } = useContext(StoreContext)
@@ -12,16 +12,25 @@ const Verify = () => {
 
   const navigate = useNavigate();
 
-  const verifyPayment = async () => {
-    const response = await axios.post(url + "/api/order/verify", { success, orderId });
-    console.log("response: ",response.data);
+const verifyPayment = async () => {
+  try {
+    const response = await axiosInstance.post(
+      "/api/order/verify",
+      { success, orderId }
+    );
+
+    console.log("response:", response.data);
+
     if (response.data.success) {
       navigate("/myorders");
+    } else {
+      navigate("/");
     }
-    else {
-      navigate("/")
-    }
+  } catch (error) {
+    console.error("Verify payment error:", error);
+    navigate("/");
   }
+};
 
   useEffect(() => {
     verifyPayment();
